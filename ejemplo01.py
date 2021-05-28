d69db71a415f1af13166da6dcb4e3e24b9bb7bed
Lechucero <yosip.urquizo@gmail.com>
2021-05-27 23:17:14 -0500

    Ejemplo-01-02

---
+++ ejemplo01.py
@@ -0,0 +1,1 @@
+print("Hola Mundo desde Python 3.9.2")
---
+++ ejemplo02.py
@@ -0,0 +1,48 @@
+# Tipos de Datos
+# String o Cadena
+# nombre = "Yosip Urquizo Gómez"
+# print("Yosip Urquizo")
+# print(nombre)
+# print("nombre")
+
+# # Integer VARIABLES
+# a=15
+# b=20
+# c=a+b
+# print(c)
+
+# # Esta forma es estatica
+#print(20+15)
+
+# # Float
+# x=15.5
+# print(x)
+
+# # # # Boolean
+# aprobado = True
+# eliminado = False
+# print(True)
+# print(False)
+
+# # List
+# print([10,15,30,45])
+# print(['curso','alumno','nota'])
+# print([10,'nota',True,15.3])
+# print([])
+
+# # # Tuples
+# print((18,20,40,50))
+# print(('curso','alumno','nota'))
+# print((10,'nota',True,15.3))
+# # print(())
+
+# # Dictionaries
+# print(
+#      {
+#          "nombre"   :   "Yosip",
+#          "apellido" :   "Urquizo",
+#          "usuario"  :   "yurquizo"
+#      })
+
+# # # # None
+# print(None)
